3bdf54ae791cf678ce73ceb4e9f69c1abe776bc7
SophiaWeiler <44530739+SophiaWeiler@users.noreply.github.com>
2021-10-20 12:55:28 -0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,84 @@ import "./styles.css"
 
 function Home() {
 	return(
-		<div className="Test">This is a test of the Home.js component. </div>
+	<!DOCTYPE html>
+		<html>
+		    <title>
+			Linguistory
+		    </title>
+		    <head>
+			<link href = "style.css">
+
+			 <!-- documentation at http://getbootstrap.com/docs/4.3/, alternative themes at https://bootswatch.com/ -->
+			 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+			 <link href="/etymLogo.png" rel="icon">
+
+		    </head>
+
+		    <body>
+		  <nav class="navbar sticky-top">
+
+
+
+			      <a class="title" href="#title"> 
+				  Linguistory</a>
+
+
+		       <a class="contact"  href="/contact">Contact</a>
+
+
+
+
+			  </nav>  
+
+
+		    <main>
+
+
+		     <div class="flex-container">
+			  <div class="flex-child">
+
+
+			    <textarea class="inputBox" id="textBox" name="textBox" rows="20" cols="50" placeholder="Type or paste text here"></textarea>
+
+			  </div>
+		<div class="flex-child">
+
+			  <header class="textAnalysis">
+			    Analysis
+			</header>
+
+			<div class="box">
+			    <ul>
+				<li class="box-percent box-percent-Romance">x % Romance</li>
+				<li class="box-percent box-percent-Germanic">y % Germanic</li>
+				<li class="box-percent box-percent-Other">z % Other</li>
+			    </ul>
+		</div>
+
+		 <header class="key">
+			      Key
+			  </header>
+
+			 <div>
+			 <ul class="list-group">
+
+		  <li class="list-group-item list-group-item-romance">Romance</li>
+		  <li class="list-group-item list-group-item-germanic">Germanic</li>
+		  <li class="list-group-item list-group-item-other">Other</li> 
+		  </ul>
+			 </div>
+			</div>
+
+			</div>
+
+
+			</main>
+
+		    </body>
+
+		</html>
 	)
 }
 
