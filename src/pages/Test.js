21e5183a66b18197f879d90aaf1728ed8aecdb20
Sean <sean14520@gmail.com>
2021-11-01 14:02:59 -0700

    Removed package-lock.json, added information to test page.

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -6,7 +6,9 @@ class Test extends React.Component {
         return(
             <div>
                 <h1>This is a test!</h1>
-
+                <div>To test the homepage, go to /home</div>
+                <div>To test the contact page, go to /contact</div>
+                <div>For updates or other URLs, consult Main.js</div>
 
             </div>
 
